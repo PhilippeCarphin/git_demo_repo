149c408aabc2f97ec56db6634f18f925d7e17cc5
Philippe Carphin <philippe.carphin@polymtl.ca>
2022-07-12 11:43:09 -0400

    Compter juste les mots qui sont juste des lettres

--- count_words.py
+++ count_words.py
@@ -1,4 +1,5 @@
 import argparse
+import re
 
 def get_args():
     p = argparse.ArgumentParser(description="Compte le nombre de mots dans un fichier texte")
@@ -12,7 +13,12 @@ def main():
         text = f.read()
 
     words = text.split()
+    nb_real_words = 0
+    for w in words:
+        if re.match(r'[a-zA-Z]+', w):
+            nb_real_words += 1
 
+    print(f"Le nombre de vrais mots dans {args.file} est {nb_real_words}")
     print(f"Le nombre de mots dans {args.file} est {len(words)}")
 
 if __name__ == '__main__':
